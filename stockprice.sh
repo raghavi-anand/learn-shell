dbbc4ffee5313e12cdcbb07dd48ef4d8ccacd25a
raghavi-anand <raghavianandkumar@gmail.com>
2024-07-30 07:25:33 +0530

    stock price

--- stockprice.sh
+++ stockprice.sh
@@ -1,1 +1,1 @@
-curl -s https://www.moneycontrol.com/india/stockpricequote/banks-private-sector/hdfcbank/HDF01 | grep nsecp
+curl -s https://www.moneycontrol.com/india/stockpricequote/banks-private-sector/hdfcbank/HDF01 | grep nsecp | awk -F '[<>]' '{print $3'
